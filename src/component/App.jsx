96b68b8acafb7df8aebbcbb13555407eefa359ba
Nishitk-20 <61048438+Nishitk-20@users.noreply.github.com>
2020-09-12 00:36:21 +0530

    Add Sample Notes-2
    Changes made  in App.jsx and Note.jsx

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
+import {note} from "../note"
 
 function App(){
     return (
-    <div>
-        <Header/>
-        <Note/>
-        <Footer/>
-    </div>
+      <div>
+          <Header />
+
+          {note.map(function (noteItem){
+            return (
+              <Note
+                key = {noteItem}
+                title = {noteItem.title}
+                content = {noteItem.content}
+              />
+            );
+
+          })}
+
+          <Footer />
+
+      </div>
     );
 }    
 
